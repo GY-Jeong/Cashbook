eff087806c5404c69a4ccf1f396345b33b010d13
GeunYoungJeong <jyjg1124@gmail.com>
2021-04-09 16:25:50 +0900

    initial commit

---
+++ CashBook/Cashbook.cpp
@@ -0,0 +1,7 @@
+#include <bits/stdc++.h>
+#include "header.hpp"
+
+void Cashbook::inputTerm()
+{
+	
+}
--- CashBook/header.hpp
+++ CashBook/header.hpp
@@ -2,7 +2,9 @@
 
 using namespace std;
 
-int main()
+class Cashbook
 {
+public:
+	void inputTerm();
 	
-}
+};
